a53cd4841cb1b959dd3fa188bcc4c0f443d0a1ba
Kenneth Doerflein <ktd500@verizon.net>
2022-03-17 17:07:48 -0400

    add change password, username scripts

---
+++ scripts/change_pass.php
@@ -0,0 +1,79 @@
+<?php 
+    //includes file with db connection
+    require_once '../db_connect.php';
+    
+    //gets session info
+    session_start();
+    
+    //takes input passed from form and assigns to variables
+    $oldPassword= trim($_POST['oldpass']);
+    $pass = trim($_POST['newpass']);
+    $confirmNewPassword = trim($_POST['connewpass']);
+    
+    $query = "SELECT * FROM CUSTOMER WHERE cUsername = '".$_SESSION['user']."'";
+    //gets info from db
+    $results = $db->query($query);
+    $row = $results->fetch_assoc();
+	$user = $row['cUsername'];
+	$cpass = $row['cPassword'];
+	$results->free();
+	
+    if (!$oldPassword || !$pass || !$confirmNewPassword) {
+	    $_SESSION['passwordChange_failed'] = 'invalid_input';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	//checks if password is at least 6 characters
+	else if (strlen($pass) < 6) {
+	    $_SESSION['passwordChange_failed'] = 'pass_short';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	else if (!password_verify($oldPassword, $cpass)) {
+	    $_SESSION['passwordChange_failed'] = 'pwdnotmatch';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	else if ($pass != $confirmNewPassword) {
+	    $_SESSION['passwordChange_failed'] = 'newpwdnotmatch';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	
+	//adds slashes for any quotes in inputs
+	if (!get_magic_quotes_gpc()) {
+        $pass = addslashes($pass);
+	}
+	
+	//hashes password
+	$pass = password_hash($pass, PASSWORD_DEFAULT);
+	
+	$sql = "UPDATE CUSTOMER SET cPassword='$pass' WHERE cUsername='$user'";
+	if ($db->query($sql) === TRUE) {
+	    $_SESSION['passwordChange_success'] = 'pass_changed';
+	    header('Location: ../account.php');
+	}
+	else{
+      $_SESSION['passwordChange_failed'] = 'updateERR';
+	    header('Location: ../account.php');
+	    //closes db conection
+	    $db->close();
+	    exit();
+    }
+    header('Location: ../account.php');
+	//closes db connection
+    $db->close();
+    exit();
+?>
---
+++ scripts/change_user.php
@@ -0,0 +1,118 @@
+<?php 
+    //includes file with db connection
+    require_once '../db_connect.php';
+    
+    //gets session info
+    session_start();
+    
+    //takes input passed from form and assigns to variables
+    $pass = trim($_POST['pass']);
+    $newUsername = strtolower(trim($_POST['newuser']));
+    
+    $query = "SELECT * FROM CUSTOMER WHERE cUsername = '".$_SESSION['user']."'";
+    //gets info from db
+    $results = $db->query($query);
+    $row = $results->fetch_assoc();
+	$user = $row['cUsername'];
+	$cpass = $row['cPassword'];
+	$custID = $row['customerID'];
+	$results->free();
+	
+    if (!$pass || !$newUsername) {
+	    $_SESSION['uNameChange_failed'] = 'invalid_input';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	else if (!password_verify($pass, $cpass)) {
+	    $_SESSION['uNameChange_failed'] = 'pwdnotmatch';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	else if ($user === $newUsername) {
+	    $_SESSION['uNameChange_failed'] = 'sameasold';
+	    header('Location: ../account.php');
+	    
+	    //closes db conection
+	    $db->close();
+	    exit();
+	}
+	
+	//adds slashes for any quotes in inputs
+	if (!get_magic_quotes_gpc()) {
+        $pass = addslashes($pass);
+        $newUsername = addslashes($newUsername);
+	}
+	
+	$query2 = 'SELECT cUsername FROM CUSTOMER';
+	$results2 = $db->query($query2);
+	$num_results = $results2->num_rows;
+    for ($i = 0; $i < $num_results; $i++) {
+        $row2 = $results2->fetch_assoc();
+        
+        //compares current usernames with new username    
+        if ($newUsername == $row2['cUsername']) {
+            //exits program is there is a match
+            $_SESSION['uNameChange_failed'] = 'usertaken';
+            header('Location: ../account.php');
+            
+            //closes db conection
+            $results->free();
+	        $db->close();
+            exit();
+        }
+        
+    }
+	
+// 	#################################################
+// 	# probably should remove and take ownerUsername out of ACCOUNTS table
+// 	$query3 = 'SELECT ownerUsername, ownerID FROM ACCOUNTS';
+// 	$results3 = $db->query($query3);
+// 	$num_results3 = $results3->num_rows;
+//     for ($i = 0; $i < $num_results3; $i++) {
+//         $row3 = $results3->fetch_assoc();
+        
+//         //compares current usernames with new username    
+//         if ($user == $row3['ownerUsername'] && $custID == $row3['ownerID']) {
+//             $sql = "UPDATE ACCOUNTS SET ownerUsername='$newUsername' WHERE ownerUsername='$user'";
+//             $db->query($sql);
+//         }
+        
+//     }
+// 	#################################################
+	
+	$sql = "UPDATE CUSTOMER SET cUsername='$newUsername' WHERE cUsername='$user'";
+	if ($db->query($sql) === TRUE) {
+	    $_SESSION['uNameChanged'] = 'usernamechanged';
+	    
+	    $_SESSION['loggedin'] = true;
+        $_SESSION['user'] = $newUsername;
+        $_SESSION['name'] = $row['cFname'];
+        
+        $_SESSION['newlog'] = true;
+        header('Location: ../account.php');
+        
+        //closes db connection
+        //$result->free();
+        //$db->close();-->
+        exit();
+	    
+	}
+	else{
+      $_SESSION['uNameChange_failed'] = 'updateERR';
+	    header('Location: ../account.php');
+	    //closes db conection
+	    //$result->free();
+	    //$db->close();
+	    exit();
+    }
+    header('Location: ../account.php');
+	//closes db connection
+    //$db->close();
+    exit();
+?>
